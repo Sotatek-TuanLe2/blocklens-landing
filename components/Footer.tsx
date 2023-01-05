61c54f6b6c259c9c8606008d901344fc7201fe38
sotatek-trungbui <trung.bui@sotatek.com>
2023-01-05 23:27:43 +0700

    feat: create header footer project

---
+++ components/Footer.tsx
@@ -0,0 +1,82 @@
+import React from 'react';
+import Link from 'next/link';
+import styles from '../styles/components/Footer.module.scss';
+
+const menus = [
+  {
+    name: 'Developer',
+    path: '/developer',
+  },
+  {
+    name: 'Documentation',
+    path: '/documentation',
+  },
+  {
+    name: 'About us',
+    path: '/about-us',
+  },
+  {
+    name: 'Pricing',
+    path: '/pricing',
+  },
+];
+
+const linkTerms = [
+  {
+    name: 'Privacy Policy',
+    path: '/privacy-policy',
+  },
+  {
+    name: 'Terms of Service',
+    path: '/terms-of-service',
+  },
+];
+
+const Footer = () => {
+  return (
+    <div className={styles['footer-landing']}>
+      <div className={styles['box-info']}>
+        <img src="/images/logo.png" alt="logo" />
+
+        <div className={styles['description']}>
+          Block Sniper pushes on-chain events to your back-end web hooks.
+          Reliable, configurable, real-time. Get started for free now!
+        </div>
+        <div className={styles['name']}>© 2022, Block Sniper</div>
+      </div>
+
+      <div className={styles['menus-link']}>
+        <div className={styles['menus']}>
+          <div>Company</div>
+          {menus.map((item, index) => {
+            return (
+              <Link
+                className={styles['menu-item']}
+                href={item.path}
+                key={index}
+              >
+                {item.name}
+              </Link>
+            );
+          })}
+        </div>
+
+        <div className={styles['menus']}>
+          {linkTerms.map((item, index) => {
+            return (
+              <Link
+                className={styles['menu-item']}
+                href={item.path}
+                key={index}
+              >
+                {item.name}
+              </Link>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
