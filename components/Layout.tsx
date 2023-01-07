a0f9406b317590df78896fe90682729055b0bcf5
tuan.le2 <tuan.le2@sotatek.com>
2023-01-07 10:35:26 +0700

    feat: add tidio live chat

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { NextSeo } from 'next-seo';
+import Script from 'next/script';
 import Footer from './Footer';
 import Header from './Header';
 import { defaultSEO } from 'next-seo.config';
+import { BLOCKSNIPER_LIVECHAT_SCRIPT } from 'utils/constant';
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -22,11 +24,16 @@ const Layout = (props: LayoutProps) => {
     return () => window.removeEventListener('scroll', handleScroll);
   }, []);
 
+  const _renderLiveChatButton = () => (
+    <Script src={BLOCKSNIPER_LIVECHAT_SCRIPT} strategy="lazyOnload" />
+  );
+
   return (
     <div className={`page-container ${className}`}>
       <NextSeo {...SEO} />
       <Header isFixedHeader={isFixedHeader} />
       <div className="content-container">{children}</div>
+      {_renderLiveChatButton()}
       <Footer />
     </div>
   );
--- utils/constant.ts
+++ utils/constant.ts
@@ -1,2 +1,4 @@
 export const BLOCKSNIPER_LOGIN_URL = 'https://blocksniper.io/login';
 export const BLOCKSNIPER_SINGUP_URL = 'https://blocksniper.io/sign-up';
+export const BLOCKSNIPER_LIVECHAT_SCRIPT =
+  '//code.tidio.co/xf1rnuwtrz8ldu8dkp2l0twgubtfztyb.js';
