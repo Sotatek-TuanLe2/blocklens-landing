f2bb583e87319fe841a74d77146726a754fb89f4
tuan.le2 <tuan.le2@sotatek.com>
2023-01-13 08:58:53 +0700

    fix: optimize config

--- next.config.js
+++ next.config.js
@@ -8,6 +8,15 @@ const nextConfig = {
     includePaths: [path.join(__dirname, 'styles')],
   },
   pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js'],
+  compiler: {
+    removeConsole: {
+      exclude: ['error'],
+    },
+  },
+  compress: true,
+  optimization: {
+    minimize: true,
+  },
 };
 
 module.exports = nextConfig;
